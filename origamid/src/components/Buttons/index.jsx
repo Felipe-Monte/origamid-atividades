fe4edeae688902aac8321d252af29f67fede81b3
felipe <carlosfelipemonte1997@hotmail.com>
2024-11-01 15:05:51 -0300

    Feat: create card product

--- origamid/src/components/Buttons/index.jsx
+++ origamid/src/components/Buttons/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Container } from './styles';
 
 const Buttons = () => {
+  function handleClick(productName) {
+    console.log(`CLicou em ${productName}`);
+  }
+
   return (
     <Container>
-      <button>Produtos</button>
-      <button>Contato</button>
+      <button onClick={() => handleClick('Produtos')}>Produtos</button>
+      <button onClick={() => handleClick('Contato')}>Contato</button>
     </Container>
   );
 };
