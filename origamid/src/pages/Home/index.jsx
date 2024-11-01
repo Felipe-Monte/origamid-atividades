de79cdbe0b06bc8cb4c3ce9c25027ef1fec846a5
felipe <carlosfelipemonte1997@hotmail.com>
2024-11-01 14:31:08 -0300

    iniciando projeto

---
+++ origamid/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Buttons from '../../components/Buttons';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Buttons />
+    </div>
+  );
+};
+
+export default Home;
